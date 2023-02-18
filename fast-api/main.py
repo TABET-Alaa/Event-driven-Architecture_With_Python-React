685acbe904e934b0714f7e767b95b73e0f0dc30e
Alaaddine TABET <alta.tabet@gmail.com>
2023-02-18 11:51:35 +0100

    update the get state and the post endpoints

--- fast-api/main.py
+++ fast-api/main.py
@@ -15,9 +15,9 @@ app.add_middleware(
 
 # instruction pour connecter a redis cloud 
 redis = get_redis_connection(
-    host="redis-12587.c250.eu-central-1-1.ec2.cloud.redislabs.com",
-    port=12587,
-    password="O2fzOTjuFjcFzCPLm5ZK6OfI4uTLcBrP",
+    host="redis-16169.c293.eu-central-1-1.ec2.cloud.redislabs.com",
+    port=16169,
+    password="SPTzjyDiawPKNjDo0Fvhf8lXFzFsP8KQ",
     decode_responses=True,
 )
 
@@ -25,8 +25,8 @@ redis = get_redis_connection(
 class Delivery(HashModel):
     budget: int = 0
     notes: str  = '' 
+
     # class meta to connect this model with redis database
-    
     class Meta :
         database = redis
         
@@ -34,16 +34,26 @@ class Delivery(HashModel):
 class Event(HashModel):
     delivery_id: str = None
     type: str  
-    data: str 
+    data: str
+
     # class meta to connect this model with redis database
-    
     class Meta :
         database = redis
-        
+     
+@app.get('/deliveries/{pk}/status')
+async def get_state(pk: str):
+     #we get the state from the cache
+     state = redis.get(f'delivery:{pk}')
+     return json.loads(state)
+
+
 @app.post('/deliveries/create')
 async def create(request: Request):
-    body = await request.json()
-    delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
-    event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
-    return event
-
+     body = await request.json()
+     delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
+     event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
+     state = consumers.create_delivery({}, event)
+     #when we create the state we will store it in the cache
+     redis.set(f'delivery:{delivery.pk}', json.dumps(state))
+     #and we returned it
+     return state
